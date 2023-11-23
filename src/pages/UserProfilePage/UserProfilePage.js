9790dd1e149c479181c499579955429a6359940c
Mohammad Najdy <mhn17@mail.aub.edu>
2023-11-23 15:53:16 +0200

    fixes

--- src/pages/UserProfilePage/UserProfilePage.js
+++ src/pages/UserProfilePage/UserProfilePage.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../UserContext.js';
 import './UserProfilePage.css';
 import defaultProfilePic from '../../components/default-profile-picture.jpg';
@@ -15,7 +15,6 @@ function UserProfilePage() {
   });
 
   const navigate = useNavigate(); // Updated to use navigate
-
 
   if (!user) {
     return <div>Loading user data...</div>;
@@ -47,22 +46,65 @@ function UserProfilePage() {
       <div className="user-profile-container">
         {isEditing ? (
           <form onSubmit={handleFormSubmit} className="edit-form">
-            {/* ... existing form fields ... */}
-            <button type="submit" className="btn save-btn">Save Changes</button>
-            <button type="button" onClick={() => setIsEditing(false)} className="btn cancel-btn">Cancel</button>
+            <div className="edit-profile-picture">
+              <label htmlFor="profilePicture">Profile Picture:</label>
+              <input
+                type="text"
+                id="profilePicture"
+                name="profilePicture"
+                value={editFormData.profilePicture}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="edit-username">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={editFormData.username}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="edit-gender">
+              <label htmlFor="gender">Gender:</label>
+              <input
+                type="text"
+                id="gender"
+                name="gender"
+                value={editFormData.gender}
+                onChange={handleInputChange}
+              />
+            </div>
+            {/* Add other fields for editing */}
+            <button type="submit" className="btn save-btn">
+              Save Changes
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsEditing(false)}
+              className="btn cancel-btn"
+            >
+              Cancel
+            </button>
           </form>
         ) : (
           <div className="user-info">
-            <img 
-              src={user.profilePicture || defaultProfilePic} 
-              alt="Profile" 
-              className="profile-picture" 
+            <img
+              src={user.profilePicture || defaultProfilePic}
+              alt="Profile"
+              className="profile-picture"
             />
             <h1 className="username">{user.username}</h1>
-            {/* ... other user details ... */}
+            <p className="gender">{user.gender}</p>
+            {/* Display other user details */}
             <div className="button-group">
-              <button className="btn edit-profile-btn" onClick={handleEditProfile}>Edit Profile</button>
-              <button className="btn watchlist-btn" onClick={goToWatchlist}>View Watchlist</button>
+              <button className="btn edit-profile-btn" onClick={handleEditProfile}>
+                Edit Profile
+              </button>
+              <button className="btn watchlist-btn" onClick={goToWatchlist}>
+                View Watchlist
+              </button>
             </div>
           </div>
         )}
@@ -71,8 +113,10 @@ function UserProfilePage() {
           <section className="user-ratings">
             <h2>Ratings</h2>
             {user.ratings && user.ratings.length > 0 ? (
-              user.ratings.map(rating => (
-                <p key={rating.id}>{rating.movieTitle}: {rating.score}/5</p>
+              user.ratings.map((rating) => (
+                <p key={rating.id}>
+                  {rating.movieTitle}: {rating.score}/5
+                </p>
               ))
             ) : (
               <p>No ratings available</p>
@@ -82,9 +126,7 @@ function UserProfilePage() {
           <section className="user-top-picks">
             <h2>Top Picks</h2>
             {user.topPicks && user.topPicks.length > 0 ? (
-              user.topPicks.map(pick => (
-                <p key={pick.id}>{pick.movieTitle}</p>
-              ))
+              user.topPicks.map((pick) => <p key={pick.id}>{pick.movieTitle}</p>)
             ) : (
               <p>No top picks available</p>
             )}
@@ -93,14 +135,16 @@ function UserProfilePage() {
           <section className="user-reviews">
             <h2>Recent Reviews</h2>
             {user.reviews && user.reviews.length > 0 ? (
-              user.reviews.map(review => (
-                <p key={review.id}>{review.movieTitle}: {review.content}</p>
+              user.reviews.map((review) => (
+                <p key={review.id}>
+                  {review.movieTitle}: {review.content}
+                </p>
               ))
             ) : (
               <p>No recent reviews available</p>
             )}
           </section>
-          </div>
+        </div>
       </div>
     </div>
   );
