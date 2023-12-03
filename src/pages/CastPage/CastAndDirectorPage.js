ed27f030b423626c4fd36c16bb5c06648e9b36fd
Zakaria Al Shahary <zma47@mail.aub.edu>
2023-12-03 15:34:30 +0200

    new

--- src/pages/CastPage/CastAndDirectorPage.js
+++ src/pages/CastPage/CastAndDirectorPage.js
@@ -4,7 +4,9 @@ import './CastAndDirectorPage.css';
 
 const CastAndDirectorPage = () => {
     const { id } = useParams();
-    const [movieDetails, setMovieDetails] = useState({ director: null, cast: [], writers: [] });
+    const [director, setDirector] = useState(null);
+    const [cast, setCast] = useState([]);
+    const [writers, setWriters] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -21,12 +23,27 @@ const CastAndDirectorPage = () => {
     };
 
     useEffect(() => {
-        const fetchMovieDetails = async () => {
+        const fetchCastDirectorAndWriters = async () => {
             try {
                 const res = await fetch(`http://localhost:5000/api/movies/${id}`);
-                if (!res.ok) throw new Error('Failed to fetch movie details');
-                const data = await res.json();
-                setMovieDetails({ director: data.director, cast: data.cast, writers: data.writers });
+                if (!res.ok) throw new Error('Failed to fetch movie data');
+                const movieData = await res.json();
+    
+                // Fetch director details
+                const directorData = await fetchDataById('directors', movieData.director);
+                setDirector(directorData);
+                console.log("Director Data:", directorData); // Log director data
+    
+                // Fetch cast (actors) details
+                const castData = await Promise.all(movieData.cast.map(actorId => fetchDataById('actors', actorId)));
+                setCast(castData);
+                console.log("Cast Data:", castData); // Log cast data
+    
+                // Fetch writers details
+                const writersData = await Promise.all(movieData.writers.map(writerId => fetchDataById('writers', writerId)));
+                setWriters(writersData);
+                console.log("Writers Data:", writersData); // Log writers data
+    
                 setLoading(false);
             } catch (err) {
                 console.error(err);
@@ -34,8 +51,8 @@ const CastAndDirectorPage = () => {
                 setLoading(false);
             }
         };
-
-        fetchMovieDetails();
+    
+        fetchCastDirectorAndWriters();
     }, [id]);
 
     if (loading) return <p>Loading...</p>;
@@ -45,24 +62,33 @@ const CastAndDirectorPage = () => {
         <div className="cast-director-page">
             <h1>Director, Cast, and Writers</h1>
             <div className="director-card">
-                {movieDetails.director && (
+                {director && (
                     <>
-                        <img src={movieDetails.director.imagePath} alt={movieDetails.director.name} />
-                        <p>Director: {movieDetails.director.name}</p>
+                        <img src={director.posterImage} alt={director.name} />
+                        <p>Director: {director.name}</p>
+                        <p>Born: {director.dateOfBirth}</p>
+                        <p>Biography: {director.biography}</p>
                     </>
                 )}
             </div>
             <div className="cast-container">
-                {movieDetails.cast.map((actor, index) => (
+                {cast.map((actor, index) => (
                     <div key={index} className="cast-card">
-                        <img src={actor.imagePath} alt={actor.name} />
-                        <p>{actor.name} as {actor.character}</p>
+                        <img src={actor.posterImage} alt={actor.name} />
+                        <p>{actor.name}</p>
+                        <p>Born: {actor.dateOfBirth}</p>
+                        <p>Bio: {actor.biography}</p>
                     </div>
                 ))}
             </div>
             <div className="writers-container">
-                {movieDetails.writers.map((writer, index) => (
+                {writers.map((writer, index) => (
+                    <div key={index} className="writer-card">
+                    <img src={writer.posterImage} alt={writer.name} />
                     <p key={index}>Writer: {writer.name}</p>
+                    <p key={index}>Born: {writer.dateOfBirth}</p>
+                    <p key={index}>Biography: {writer.biography}</p>
+                    </div>
                 ))}
             </div>
         </div>
